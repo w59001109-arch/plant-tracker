00a5db2e3e23335e5ccb34532c9031eab71ef3b1
w59001109-arch <w59001109@gmail.com>
2026-05-10 14:32:32 +0800

    Fix PWA identity conflict with other tools on same domain
    Add id and scope to manifest.json so Chrome treats plant-tracker
    as a distinct PWA, separate from other apps on w59001109-arch.github.io

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'plant-journal-v1.1';
+const CACHE = 'plant-journal-v1.2';
 const ASSETS = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', e => {
