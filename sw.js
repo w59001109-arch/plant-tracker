b743147e2c4194ba96a11fb4d3944b20fbb5e7dc
ca <ca@cadeMac-mini.local>
2026-05-10 17:10:17 +0800

    fix: safe cache cleanup, bump to v1.3

--- sw.js
+++ sw.js
@@ -1,18 +1,17 @@
-const CACHE = 'plant-journal-v1.2';
-const ASSETS = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png'];
+const CACHE = 'plant-journal-v1.3';
+const OLD = ['plant-journal-v1', 'plant-journal-v1.2'];
+const ASSETS = ['/plant-tracker/', '/plant-tracker/index.html', '/plant-tracker/manifest.json', '/plant-tracker/icon-192.png', '/plant-tracker/icon-512.png'];
 
 self.addEventListener('install', e => {
-  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
-  self.skipWaiting();
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)).then(() => self.skipWaiting()));
 });
 
 self.addEventListener('activate', e => {
   e.waitUntil(
     caches.keys().then(keys =>
-      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
-    )
+      Promise.all(keys.filter(k => OLD.includes(k)).map(k => caches.delete(k)))
+    ).then(() => self.clients.claim())
   );
-  self.clients.claim();
 });
 
 self.addEventListener('fetch', e => {
@@ -20,10 +19,7 @@ self.addEventListener('fetch', e => {
   e.respondWith(
     fetch(e.request)
       .then(res => {
-        if (res.ok) {
-          const clone = res.clone();
-          caches.open(CACHE).then(c => c.put(e.request, clone));
-        }
+        if (res.ok) caches.open(CACHE).then(c => c.put(e.request, res.clone()));
         return res;
       })
       .catch(() => caches.match(e.request))
